e984ea0ae19b83ec439a034f4d321603e6911eaf
Matheus Santos <wenked.matheus@yahoo.com.br>
2022-03-16 11:00:35 -0300

    cv2 teste

--- main.py
+++ main.py
@@ -3,10 +3,91 @@ import pyautogui
 import logging
 from cv2 import cv2
 from os import listdir
+from random import randint
+from random import random
+import mss
+import numpy as np
 
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
 pyautogui.PAUSE = 5
+
+def addRandomness(n, randomn_factor_size=None):
+    """Returns n with randomness
+    Parameters:
+        n (int): A decimal integer
+        randomn_factor_size (int): The maximum value+- of randomness that will be
+            added to n
+
+    Returns:
+        int: n with randomness
+    """
+
+    if randomn_factor_size is None:
+        randomness_percentage = 0.1
+        randomn_factor_size = randomness_percentage * n
+
+    random_factor = 2 * random() * randomn_factor_size
+    if random_factor > 5:
+        random_factor = 5
+    without_average_random_factor = n - randomn_factor_size
+    randomized_n = int(without_average_random_factor + random_factor)
+    return int(randomized_n)
+
+def moveToWithRandomness(x,y,t):
+    pyautogui.moveTo(addRandomness(x,10),addRandomness(y,10),t+random()/2)
+    
+def printScreen():
+    with mss.mss() as sct:
+        monitor = sct.monitors[0]
+        sct_img = np.array(sct.grab(monitor))
+        
+        return sct_img[:,:,:3]
+    
+def positions(target, threshold=0.7,img = None):
+    if img is None:
+        img = printScreen()
+    result = cv2.matchTemplate(img,target,cv2.TM_CCOEFF_NORMED)
+    w = target.shape[1]
+    h = target.shape[0]
+
+    yloc, xloc = np.where(result >= threshold)
+
+
+    rectangles = []
+    for (x, y) in zip(xloc, yloc):
+        rectangles.append([int(x), int(y), int(w), int(h)])
+        rectangles.append([int(x), int(y), int(w), int(h)])
+
+    rectangles, weights = cv2.groupRectangles(rectangles, 1, 0.2)
+    return rectangles
+    
+def clickBtn(img, timeout=3, threshold = 0.7):
+    """Search for img in the scree, if found moves the cursor over it and clicks.
+    Parameters:
+        img: The image that will be used as an template to find where to click.
+        timeout (int): Time in seconds that it will keep looking for the img before returning with fail
+        threshold(float): How confident the bot needs to be to click the buttons (values from 0 to 1)
+    """
+
+    
+    start = time.time()
+    has_timed_out = False
+    while(not has_timed_out):
+        matches = positions(img, threshold=threshold)
+
+        if(len(matches)==0):
+            has_timed_out = time.time()-start > timeout
+            continue
+
+        x,y,w,h = matches[0]
+        pos_click_x = x+w/2
+        pos_click_y = y+h/2
+        moveToWithRandomness(pos_click_x,pos_click_y,1)
+        pyautogui.click()
+        return True
+
+    return False    
 
 
 def click_on_image(image_name,process,delay=10):
